28be987831db3d921d439f0fe6d7079a6869e1e6
peiwen.liu <peiwen.liu@picovr.com>
2020-03-20 14:28:43 +0800

    add rxjava tools

---
+++ pwtools/src/main/java/cn/qd/peiwen/pwtools/rxjava/ResponseObserver.java
@@ -0,0 +1,41 @@
+package cn.qd.peiwen.pwtools.rxjava;
+
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Created by nick on 2017/9/5.
+ */
+
+public abstract class ResponseObserver<T> implements Observer<T> {
+    @Override
+    public void onSubscribe(Disposable d) {
+        this.onStart();
+    }
+
+    @Override
+    public void onNext(T entity) {
+        this.onSuccess(entity);
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        e.printStackTrace();
+        this.onFailure(e);
+        this.onCompleted();
+    }
+
+    @Override
+    public void onComplete() {
+        this.onCompleted();
+    }
+
+    protected abstract void onStart();
+
+    protected abstract void onSuccess(T entity);
+
+    protected abstract void onFailure(Throwable e);
+
+    protected abstract void onCompleted();
+}
