d0db1cc16c3d1bd7804dd372aa7957431469c30c
peiwen.liu <peiwen.liu@picovr.com>
2020-03-26 09:45:44 +0800

    Add isShouldHideKeyboard

--- pwtools/src/main/java/cn/qd/peiwen/pwtools/KeyboardUtils.java
+++ pwtools/src/main/java/cn/qd/peiwen/pwtools/KeyboardUtils.java
@@ -2,8 +2,10 @@ package cn.qd.peiwen.pwtools;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 /**
  * Created by nick on 2017/8/30.
@@ -55,4 +57,20 @@ public class KeyboardUtils {
         return inputmanger.isActive();
     }
 
+    /**
+     * 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘，因为当用户点击Et时则不能隐藏
+     */
+    public static boolean isShouldHideKeyboard(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] l = {0, 0};
+            v.getLocationOnScreen(l);
+            int left = l[0], top = l[1], bottom = top + v.getHeight(), right = left + v.getWidth();
+            if (event.getRawX() > left && event.getRawX() < right && event.getRawY() > top && event.getRawY() < bottom) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
