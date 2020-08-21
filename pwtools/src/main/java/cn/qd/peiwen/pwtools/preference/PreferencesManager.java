4572cc3e4c2c6eb6476b225e55165d3d2c40208a
bang.liu <bang.liu@haierbiomedical.com>
2020-08-21 18:16:11 +0800

    Update PreferencesManager

--- pwtools/src/main/java/cn/qd/peiwen/pwtools/preference/PreferencesManager.java
+++ pwtools/src/main/java/cn/qd/peiwen/pwtools/preference/PreferencesManager.java
@@ -19,85 +19,95 @@ public class PreferencesManager {
         this.editor = this.preferences.edit();
     }
 
-    public void clear(){
+    public void clear() {
         this.editor.clear().commit();
     }
 
-    public void remove(String key){
+    public boolean isEmpty() {
+        return (0 == this.preferences.getAll().size());
+    }
+
+    public boolean isNotEmpty() {
+        return !this.isEmpty();
+    }
+
+    public void remove(String key) {
         this.editor.remove(key).commit();
     }
 
+    public void contains(String key) {
+        this.preferences.contains(key);
+    }
 
     public void putInt(String key, int value) {
-        this.editor.putInt(key,value).commit();
+        this.editor.putInt(key, value).commit();
     }
 
     public int getInt(String key) {
-        return this.preferences.getInt(key,0);
+        return this.preferences.getInt(key, 0);
     }
 
     public int getInt(String key, int defaultValue) {
-        return this.preferences.getInt(key,defaultValue);
+        return this.preferences.getInt(key, defaultValue);
     }
 
     public void putFloat(String key, float value) {
-        this.editor.putFloat(key,value).commit();
+        this.editor.putFloat(key, value).commit();
     }
 
     public float getFloat(String key) {
-        return this.preferences.getFloat(key,0.f);
+        return this.preferences.getFloat(key, 0.f);
     }
 
     public float getFloat(String key, float defaultValue) {
-        return this.preferences.getFloat(key,defaultValue);
+        return this.preferences.getFloat(key, defaultValue);
     }
 
     public void putLong(String key, long value) {
-        this.editor.putLong(key,value).commit();
+        this.editor.putLong(key, value).commit();
     }
 
     public long getLong(String key) {
-        return this.preferences.getLong(key,0L);
+        return this.preferences.getLong(key, 0L);
     }
 
     public long getLong(String key, long defaultValue) {
-        return this.preferences.getLong(key,defaultValue);
+        return this.preferences.getLong(key, defaultValue);
     }
 
     public void putBoolean(String key, boolean value) {
-        this.editor.putBoolean(key,value).commit();
+        this.editor.putBoolean(key, value).commit();
     }
 
     public boolean getBoolean(String key) {
-        return this.preferences.getBoolean(key,false);
+        return this.preferences.getBoolean(key, false);
     }
 
     public boolean getBoolean(String key, boolean defaultValue) {
-        return this.preferences.getBoolean(key,defaultValue);
+        return this.preferences.getBoolean(key, defaultValue);
     }
 
     public void putString(String key, String value) {
-        this.editor.putString(key,value).commit();
+        this.editor.putString(key, value).commit();
     }
 
     public String getString(String key) {
-        return this.preferences.getString(key,null);
+        return this.preferences.getString(key, null);
     }
 
     public String getString(String key, String defaultValue) {
-        return this.preferences.getString(key,defaultValue);
+        return this.preferences.getString(key, defaultValue);
     }
 
-
     public void putStringSet(String key, Set<String> value) {
-        this.editor.putStringSet(key,value).commit();
+        this.editor.putStringSet(key, value).commit();
     }
 
     public Set<String> getStringSet(String key) {
-        return this.preferences.getStringSet(key,null);
+        return this.preferences.getStringSet(key, null);
     }
 
     public Set<String> getStringSet(String key, Set<String> defaultValue) {
-        return this.preferences.getStringSet(key,defaultValue);
+        return this.preferences.getStringSet(key, defaultValue);
     }
 }
